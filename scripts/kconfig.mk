e279387332ebeac6dab9a3a568bb6dabe1308cb6
ShadowStar <orphen.leiliu@gmail.com>
2022-03-24 13:52:32 +0800

    Fix (scripts/kconfig.mk): Correct $(SILENT) and @, and remove unused 			  `no-dot-cfg-targets'

--- scripts/kconfig.mk
+++ scripts/kconfig.mk
@@ -2,7 +2,7 @@ KCONFIG_DIR := $(TOPDIR)/scripts/kconfig
 KCONFIG_TOP := $(TOPDIR)/Config.in
 NEED_DOT_CFG := yes
 
-no-dot-cfg-targets += menuconfig nconfig defconfig kconfig.clean
+no-dot-cfg-targets += menuconfig kconfig.clean
 
 ifneq ($(filter $(no-dot-cfg-targets),$(MAKECMDGOALS)),)
   ifeq ($(filter-out $(no-dot-cfg-targets),$(MAKECMDGOALS)),)
@@ -20,7 +20,7 @@ $(DOT_CFG) $(TOPDIR)/include/config/auto.conf.cmd: ;
 $(TOPDIR)/include/config/auto.conf: $(KCONFIG_TOP) $(DOT_CFG) $(KCONFIG_DIR)/conf
 
 $(TOPDIR)/include/config/auto.conf: $(TOPDIR)/include/config/auto.conf.cmd
-	$(SILENT)$(KCONFIG_DIR)/conf --silentoldconfig $(KCONFIG_TOP)
+	@$(KCONFIG_DIR)/conf --silentoldconfig $(KCONFIG_TOP)
 
 $(TOPDIR)/include/autoconf.h: $(TOPDIR)/include/config/auto.conf
 endif
@@ -28,14 +28,11 @@ endif
 include $(TOPDIR)/scripts/kconfig/kconfig.mk
 
 kconfig.clean:
-	-$(SILENT)rm -rf include/config/*
-	-$(SILENT)rm -f include/autoconf.h
+	-@rm -rf include/config/*
+	-@rm -f include/autoconf.h
 
 kconfig.distclean: kconfig.clean
-	-$(SILENT)rm -f $(KCONFIG_DIR)/*.o $(KCONFIG_DIR)/*.dep
-	-$(SILENT)rm -f $(KCONFIG_DIR)/zconf.*.c
-	-$(SILENT)rm -f $(KCONFIG_DIR)/lxdialog/*.o $(KCONFIG_DIR)/lxdialog/*.dep
-	-$(SILENT)rm -f $(KCONFIG_DIR)/conf $(KCONFIG_DIR)/mconf $(KCONFIG_DIR)/nconf
-
-clean: kconfig.clean
-distclean: kconfig.distclean
+	-@rm -f $(KCONFIG_DIR)/*.o $(KCONFIG_DIR)/*.dep
+	-@rm -f $(KCONFIG_DIR)/zconf.*.c
+	-@rm -f $(KCONFIG_DIR)/lxdialog/*.o $(KCONFIG_DIR)/lxdialog/*.dep
+	-@rm -f $(KCONFIG_DIR)/conf $(KCONFIG_DIR)/mconf $(KCONFIG_DIR)/nconf
