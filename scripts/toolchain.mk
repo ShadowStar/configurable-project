27aa97530386d40afc43860cc382ddfc5cadd6d6
ShadowStar <orphen.leiliu@gmail.com>
2022-03-26 19:07:22 +0800

    Fix (Toolchain): Export utils of LLVM without cross-compile-prefix
    The Clang/LLVM is natively a cross-compiler.

--- scripts/toolchain.mk
+++ scripts/toolchain.mk
@@ -1,7 +1,7 @@
 include $(TOPDIR)/include/config/auto.conf
 
 TOOLCHAIN_UTILS := ar as gcc cpp ld nm objcopy objdump ranlib strip
-TOOLCHAIN_UTILS += llvm-ar llvm-as clang clang-cpp llvm-nm llvm-objcopy llvm-objdump llvm-ranlib llvm-strip
+LLVM_UTILS += llc llvm-ar llvm-as clang clang-cpp llvm-nm llvm-objcopy llvm-objdump llvm-ranlib llvm-strip
 
 _CROSS := $(shell echo $(CFG_CROSS_TOOLCHAIN))
 
@@ -16,6 +16,8 @@ endef
 $(foreach t,$(TOOLCHAIN_UTILS),$(eval export $(call _upper,$(t)) := $(_CROSS)$(t)))
 export CC := $(GCC)
 
+$(foreach t,$(LLVM_UTILS),$(eval export $(call _upper,$(t)) := $(t)))
+
 CFLAGS := $(shell echo $(CFG_TARGET_CFLAGS))
 
 ifneq ($(call _str,$(CFG_SYSROOT_PATH)),)
@@ -28,8 +30,6 @@ ifneq ($(call _str,$(CFG_TOOLCHAIN_PATH)),)
   PATH := $(_TC_PATH)/bin:$(PATH)
   export SHELL PATH
 endif
-
-export AR AS CC CPP GCC CLANG LD NM OBJCOPY OBJDUMP RANLIB STRIP CFLAGS
 
 ifeq ($(CFG_TARGET_ARCH),"")
   CFG_TARGET_ARCH=$(shell $(GCC) -dumpmachine | cut -d'-' -f 1)
