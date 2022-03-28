60b316ac00bfa503bc05e74380fc0f8869982b18
ShadowStar <orphen.leiliu@gmail.com>
2022-03-28 21:12:54 +0800

    Feature (net.h): Define std types for BPF target

--- include/net.h
+++ include/net.h
@@ -18,7 +18,18 @@
 
 #ifndef __NET_H
 #define __NET_H
+#ifndef __BPF__
 #include <stdint.h>
+#else   /** __BPF__ */
+typedef __INT8_TYPE__ int8_t;
+typedef __INT16_TYPE__ int16_t;
+typedef __INT32_TYPE__ int32_t;
+typedef __INT64_TYPE__ int64_t;
+typedef __UINT8_TYPE__ uint8_t;
+typedef __UINT16_TYPE__ uint16_t;
+typedef __UINT32_TYPE__ uint32_t;
+typedef __UINT64_TYPE__ uint64_t;
+#endif
 
 #ifndef __BIG_ENDIAN
 #define __BIG_ENDIAN        4321
