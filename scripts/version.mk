8848b61084a0f69f654d74f6c579ed06df026259
ShadowStar <orphen.leiliu@gmail.com>
2025-03-06 13:55:22 +0800

    Feature (scripts/version.mk): Update `version.h' only if commit update

--- scripts/version.mk
+++ scripts/version.mk
@@ -1,20 +1,20 @@
 PRJ_VERSION := $(shell $(TOPDIR)/scripts/git-vergen -c -d $(TOPDIR))
 PRJ_VERSION_FULL := $(shell $(TOPDIR)/scripts/git-vergen -f -n -d $(TOPDIR))
 PRJ_GIT_SHA1 := $(shell GIT_WORK_TREE=$(TOPDIR) git show-ref -h -s 2>/dev/null | head -n1 | cut -c 1-16)
+PRJ_VERSION_H := $(TOPDIR)/include/version.h
 
 export PRJ_VERSION PRJ_VERSION_FULL
 
-no-dot-cfg-targets += $(TOPDIR)/include/version.h version
+no-dot-cfg-targets += $(PRJ_VERSION_H) version
 
-$(TOPDIR)/include/version.h: FORCE
-	@echo "#define PRJ_VERSION \"$(PRJ_VERSION)\"" > $@
-	@echo "#define PRJ_VERSION_FULL \"$(PRJ_VERSION_FULL)\"" >> $@
-	@echo "#define PRJ_GIT_SHA1 0x$(PRJ_GIT_SHA1)" >> $@
+$(PRJ_VERSION_H): FORCE
+	@if [ ! -f $@ ] || [ $(shell cat $@ | grep "\<PRJ_VERSION\>" | cut -d' ' -f3) != "$(PRJ_VERSION)" ]; then \
+		echo "#define PRJ_VERSION \"$(PRJ_VERSION)\"" > $@; \
+		echo "#define PRJ_VERSION_FULL \"$(PRJ_VERSION_FULL)\"" >> $@; \
+		echo "#define PRJ_GIT_SHA1 0x$(PRJ_GIT_SHA1)" >> $@; fi
 
-version:
-	@echo "PRJ_VERSION:      $(PRJ_VERSION)"
-	@echo "PRJ_VERSION_FULL: $(PRJ_VERSION_FULL)"
-	@echo "PRJ_GIT_SHA1:     $(PRJ_GIT_SHA1)"
+version: $(PRJ_VERSION_H)
+	@cat $^
 
 version.clean:
 	-@rm -f $(TOPDIR)/include/version.h
